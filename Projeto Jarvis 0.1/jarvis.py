563fb05434b5248bfe1855a8c9bcd28eaae1b681
knopfgit <githubknopf@gmail.com>
2026-05-02 22:41:39 -0300

    Corrigindo lógica e aplicando mais funções importantes

--- Projeto Jarvis 0.1/jarvis.py
+++ Projeto Jarvis 0.1/jarvis.py
@@ -1,10 +1,31 @@
-import os
+#Sessão responsável por importar as bibiotecas que irei utilizar no projeto
+import os # responsável por ajudar o assistente a algumas funções do sistema operacional
+import webbrowser # responsável por ajudar o assistente a realizar pesquisas web 
 
+# =====================================================================================================  
+# # Sessão inicial onde implementei as principais funções do nosso JARVIS
 def apresentacao(): 
     print("Boas-Vindas Senhor, eu sou o JARVIS, como posso ajuda-lo?")
 def ouvir():
     comando = input("Digite o seu comando:")
-    print(comando) 
-
+    return comando.lower()
+def responder_comando(comando):
+    if "olá jarvis" in comando:
+        print("Olá senhor.")
+    elif "horas" in comando:
+        print("Agora são x horas, senhor.")
+    elif "sair" in comando:
+        print ("Desligando sistema, até breve, senhor!")
+    else:
+        print("Desculpe, senhor, não entendi seu comando.")
+def iniciar():
+    apresentacao() 
+    while True:
+        comando = ouvir()
+        responder_comando(comando)
+        if comando == "sair":
+            break
+# ======================================================================================================          
+# Bloco utilizado para chamar as fuções de apresentação, e a inicialização do JARVIS
 if __name__ == "__main__":
-    apresentacao()
+    iniciar() # A função iniciar, já vem com a parte da apresentação, por isso removi e deixei apenas o iniciar
