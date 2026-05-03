f3367dea14f3acd2814a23976b1acf2528a6751f
knopfgit <githubknopf@gmail.com>
2026-05-02 22:13:24 -0300

    adicionando cabeçalhos para facilitar divisão do código

---
+++ Projeto Jarvis 0.1/jarvis.py
@@ -0,0 +1,10 @@
+import os
+
+def apresentacao(): 
+    print("Boas-Vindas Senhor, eu sou o JARVIS, como posso ajuda-lo?")
+def ouvir():
+    comando = input("Digite o seu comando:")
+    print(comando) 
+
+if __name__ == "__main__":
+    apresentacao()
