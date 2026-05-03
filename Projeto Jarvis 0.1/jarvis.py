83930f7100ecf4dfc55160ee7bd3bd70f494d409
knopfgit <githubknopf@gmail.com>
2026-05-02 23:36:45 -0300

    adicionando voz, e realizando tratamentos simples

--- Projeto Jarvis 0.1/jarvis.py
+++ Projeto Jarvis 0.1/jarvis.py
@@ -1,33 +1,39 @@
 #Sessão responsável por importar as bibiotecas que irei utilizar no projeto
 import os # responsável por ajudar o assistente a algumas funções do sistema operacional
 import webbrowser # responsável por ajudar o assistente a realizar pesquisas web 
+import pyttsx3 # biblioteca responsável pela voz do nosso JARVIS
+engine = pyttsx3.init()
+engine.setProperty("rate", 160)
 
 # =====================================================================================================  
 # # Sessão inicial onde implementei as principais funções do nosso JARVIS
 def apresentacao(): 
-    print("Boas-Vindas Senhor, eu sou o JARVIS, como posso ajuda-lo?")
+    falar("Boas-Vindas Senhor, eu sou o JARVIS, como posso ajuda-lo?")
 def ouvir():
     comando = input("Digite o seu comando:")
     return comando.lower()
+def falar(texto):
+    engine.say(texto)
+    engine.runAndWait()
 def responder_comando(comando):
     if "olá jarvis" in comando:
-        print("Olá senhor.")
+        falar("Olá senhor.")
     elif "horas" in comando:
-        print("Agora são x horas, senhor.")
+        falar("Agora são x horas senhor.")
     elif "sair" in comando:
-        print ("Desligando sistema, até breve, senhor!")
+        falar("Desligando sistema, até breve senhor!")
     elif "youtube" in comando:
-        print("Abrindo o youtube senhor.")
+        falar("Abrindo o youtube senhor.")
         webbrowser.open("https://www.youtube.com")
     elif "google" in comando:
-        print("Abrindo o google em seu navegador, senhor.")
+        falar("Abrindo o google em seu navegador senhor.")
         webbrowser.open("https://www.google.com")
     elif "pesquisar" in comando:
         termo = comando.replace("pesquisar", "").strip() # linha responsável por separar o termo que o usúario deseja pesquisar usando .strip e concatenação de "strings"
-        print("Pesquisando:" + termo + " no seu navegador, senhor")
+        falar("Pesquisando:" + termo + " no seu navegador senhor")
         webbrowser.open("https://www.google.com/search?q="+ termo)
     else:
-        print("Desculpe, senhor, não entendi seu comando.")
+        falar("Desculpe, senhor não entendi seu comando.")
 def iniciar():
     apresentacao() 
     while True:
